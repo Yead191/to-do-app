2cf1a1dc1c5a990330a182af6bc204a0b7f006eb
Yead <yead19@gmail.com>
2025-02-21 01:14:35 +0600

    firebase

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,12 @@
+import Dashboard from '@/dasboard/Dashboard';
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <Dashboard></Dashboard>
+        </div>
+    );
+};
+
+export default Root
